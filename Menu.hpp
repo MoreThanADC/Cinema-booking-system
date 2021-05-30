668c4e7bde47afdebe1300d98de5a2dc37baacc1
Artur Michałowski <artur.michalowski93@gmail.com>
2021-05-30 15:35:29 +0200

    Change filenames and includes

---
+++ Menu.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+
+void printMenu();
+int selectAction();
---
+++ Seat.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Seat {
+public: 
+    // c-tors
+    Seat();
+    Seat(std::string, std::string, size_t, bool);
+
+    // getters 
+    std::string getName() const { return name_; };
+    std::string getSurname() const { return surname_; };
+    size_t getId() const { return seatId_; };
+    bool isReserved() const { return isReserved_; };
+
+    // member functions
+    void reserve();
+
+private:
+    std::string name_;
+    std::string surname_;
+    size_t seatId_;
+    bool isReserved_ = false;
+};
