ca9c3d84d4c1d2a37a5e05bc4d7b085a938f495d
Artur Michałowski <artur.michalowski93@gmail.com>
2021-05-30 15:15:13 +0200

    Added getters to Seat.hpp

--- menu.hpp
+++ menu.hpp
@@ -1,6 +1,7 @@
 #pragma once
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
+
 
 void printMenu();
 int selectAction();
--- seat.hpp
+++ seat.hpp
@@ -4,14 +4,22 @@
 
 class Seat {
 public: 
+    // c-tors
     Seat();
     Seat(std::string, std::string, size_t, bool);
 
+    // getters 
+    std::string getName() const { return name_; };
+    std::string getSurname() const { return surname_; };
+    size_t getId() const { return seatId_; };
+    bool isReserved() const { return isReserved_; };
+    
+    // member functions
     void reserve();
 
-public:
-    std::string name_;
-    std::string surname_;
-    size_t seatId_;
+private:
+    const std::string name_;
+    const std::string surname_;
+    const size_t seatId_;
     bool isReserved_;
 };
